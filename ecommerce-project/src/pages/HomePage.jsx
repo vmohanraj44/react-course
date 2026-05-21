14adb1efdaa96ef446deca1697b5009142a72d42
vmohanraj44 <vmohanraj44@gmail.com>
2026-05-21 22:14:00 +0530

    fetch data from backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
+fetch('http://localhost:3000/api/products')
+ .then((response)=>{
+   return response.json()
+   }).then((data)=>{
+   console.log(data);
+   });
+
   return (
     <>
       <Header />
