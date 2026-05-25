566354875867b37ff7155f0223e3e16d3a86d79c
vmohanraj44 <vmohanraj44@gmail.com>
2026-05-25 07:11:37 +0530

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,73 +1,61 @@
-import { it, expect, describe, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import {userEvent} from '@testing-library/user-event';
-import axios from 'axios';
-import { Product } from './Product';
+import { it, expect, describe, vi, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+//import axios from "axios";
+import { Product } from "./Product";
 
+vi.mock("axios");
 
-vi.mock('axios');
+let product;
+let loadCart;
 
-describe('formart', ()=>{
-  it('displays the product details correctly', ()=>{
-    const product = {
+beforeEach(() => {
+  product = {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
       stars: 4.5,
-      count: 87
+      count: 87,
     },
     priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
+    keywords: ["socks", "sports", "apparel"],
   };
-     const loadCart = vi.fn(); //mock function
+  loadCart = vi.fn(); //mock function
+});
 
-      render(<Product product={product} loadCart ={loadCart} />);
+describe("formart", () => {
+  it("displays the product details correctly", () => {
+    render(<Product product={product} loadCart={loadCart} />);
 
-      expect(
-        screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
-      ).toBeInTheDocument();
+    expect(
+      screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs"),
+    ).toBeInTheDocument();
 
-      expect(
-        screen.getByText('$10.90')
-      ).toBeInTheDocument();
+    expect(screen.getByText("$10.90")).toBeInTheDocument();
 
-       expect(
-        screen.getByTestId('product-image')
-      ).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
+    expect(screen.getByTestId("product-image")).toHaveAttribute(
+      "src",
+      "images/products/athletic-cotton-socks-6-pairs.jpg",
+    );
 
-       expect(
-        screen.getByTestId('product-rating-stars')
-      ).toHaveAttribute('src', 'images/ratings/rating-45.png');
+    expect(screen.getByTestId("product-rating-stars")).toHaveAttribute(
+      "src",
+      "images/ratings/rating-45.png",
+    );
 
-       expect(
-        screen.getByText('87')
-      ).toBeInTheDocument();
-
+    expect(screen.getByText("87")).toBeInTheDocument();
   });
 
   //integration testing
-  it('add products to cart',async ()=>{
-      const product = {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  };
-     const loadCart = vi.fn(); //mock function
+  it("add products to cart", async () => {
+    render(<Product product={product} loadCart={loadCart} />);
 
-      render(<Product product={product} loadCart ={loadCart} />);
+    const user = userEvent.setup();
+    const addToClickButton = screen.getByTestId("add-to-cart-button");
+    await user.click(addToClickButton);
 
-      const user = userEvent.setup();
-      const addToClickButton = screen.getByTestId('add-to-cart-button')
-      await user.click(addToClickButton);
-
-   /*  expect(axios.post).toHaveBeenCalledWith(
+    /*  expect(axios.post).toHaveBeenCalledWith(
   '/api/cart-items',
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -75,6 +63,6 @@ describe('formart', ()=>{
   },
   expect.anything()
 );*/
-      expect(loadCart).toHaveBeenCalled();
+    expect(loadCart).toHaveBeenCalled();
   });
-})
+});
